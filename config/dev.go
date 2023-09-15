701e5601f6595a769c216422e49a8fc9e4101533
flipped <1123630646@qq.com>
2023-09-15 09:38:14 +0800

    修改dev数据库配置

--- config/dev.go
+++ config/dev.go
@@ -4,9 +4,9 @@ package config
 
 var Config = config{
 	DB: DBConfig{
-		DSN: "root:123456@tcp(192.168.30.111:3306)/webook",
+		DSN: "root:123456@tcp(192.168.137.133:3306)/webook",
 	},
 	Redis: RedisConfig{
-		Addr: "192.168.30.111:6379",
+		Addr: "192.168.137.133:6379",
 	},
 }
